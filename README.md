d49b7592e2afe12c29887cf67fb10eadac32d5ec
Jared <jaredgin89@gmail.com>
2018-05-12 12:22:28 -0400

    New commit for what was originally planned

--- README.md
+++
@@ -1,3 +0,0 @@
-# rockPaperScissors
-The Odin Project
-The goal of this project is to use HTML, CSS, and Javascript to create a game of Rock, Paper, Scissors.
---
+++ script.js
@@ -0,0 +1,93 @@
+let cChoice;
+let pChoice;
+let match = 0;
+let pWins = 0;
+let cWins = 0;
+
+function playerChoice(input)
+{
+  if(input == "rock")
+  {
+    document.getElementById("playerChoice").innerHTML = "Rock";
+    pChoice = "r";
+  }
+  else if(input == "paper")
+  {
+    document.getElementById("playerChoice").innerHTML = "Paper";
+    pChoice = "p";
+  }  else if(input == "scissors")
+  {
+    document.getElementById("playerChoice").innerHTML = "Scissors";
+    pChoice = "s";
+  }
+  compChoice();
+}
+
+function compChoice()
+{
+  let comp = Math.floor(Math.random()*3);
+  if(comp == "0")
+  {
+    document.getElementById("compChoice").innerHTML = "Rock";
+    cChoice = "r";
+  }  else if(comp == "1")
+  {
+    document.getElementById("compChoice").innerHTML = "Paper";
+    cChoice = "p";
+  }  else if(comp == "2")
+  {
+    document.getElementById("compChoice").innerHTML = "Scissors";
+    cChoice = "s";
+  }
+  roundWinner(pChoice, cChoice);
+}
+
+function roundWinner(pChoice, cChoice)
+{
+  let round = pChoice + cChoice;
+  if(round == "rr" || round == "pp" || round == "ss")
+    document.getElementById("results").innerHTML = "Tie";
+  else if(round == "sp" || round == "pr" || round == "rs")
+  {
+    document.getElementById("results").innerHTML = "Player Wins";
+    pWins += 1;
+    document.getElementById("yourScore").innerHTML = pWins;
+  }
+  else if(round == "ps" || round == "rp" || round == "sr")
+  {
+    document.getElementById("results").innerHTML = "Computer Wins";
+    cWins += 1;
+    document.getElementById("compScore").innerHTML = cWins;
+  }
+  gameMatch();
+}
+
+function gameMatch()
+{
+  match++;
+  document.getElementById("match").innerHTML = "Match: " + match;
+  if(pWins > 4 || cWins > 4)
+    winner();
+}
+
+function winner(pWins, cWins)
+{
+  if(pWins > cWins)
+    alert("Player wins!");
+  else
+    alert("Player loses.");
+  reset();
+}
+
+function reset()
+{
+  document.getElementById("match").innerHTML = "Match: " + 0;
+  document.getElementById("yourScore").innerHTML = 0;
+  document.getElementById("compScore").innerHTML = 0;
+  document.getElementById("compChoice").innerHTML = " ";
+  document.getElementById("playerChoice").innerHTML = " ";
+  document.getElementById("results").innerHTML = " ";
+  pWins = 0;
+  cWins = 0;
+  match = 0;
+}
